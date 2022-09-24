ffb1844087d7bfbed4e75cd5c039caad13f6bae1
by1991 <bduaa@connect.ust.hk>
2022-09-24 09:39:19 +0800

    create react frontend template

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # demyst-test
+https://github.com/vercel/next.js/blob/canary/examples/with-react-hook-form/pages/index.js
---
+++ demyst-frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import MaterialFormComponent from './components/MaterialFormComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">
+
+      </div>
+      <div>
+        <MaterialFormComponent />
+      </div>
+    </div>
+  );
+}
+
+export default App;
